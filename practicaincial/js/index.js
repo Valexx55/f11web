acbf90c5bf7a2241de428c0593e7cbb0dd546bc8
Val <valexx55@gmail.com>
2026-05-12 14:53:08 +0200

    js básico

---
+++ practicaincial/js/index.js
@@ -0,0 +1,5 @@
+//window es una variable
+//predefinida que representa la ventana del navegador
+//y alert, es una función de window, que permite
+//mostrar mensajes en formato pop up
+window.alert("HOLA SOCIO"); // esto es un mensaje que sale por la ventana pop up
