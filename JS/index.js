5c935e18b78300579b792434d3ebb5b4d26cf6e4
Val <valexx55@gmail.com>
2026-05-13 12:08:03 +0200

    saludar js

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+//VARIABLES PREDEFINIDAS
+//window
+//console
+
+//variables PROPIAS
+//número de goles a favor
+//número de goles en contra
+//trabajdor
+
+//FUNCIONES
+//alert
+//log
+
+console.log("HOLA desde fuera");
+
+function saludar ()
+{
+    console.log("Hola desde adentro");
+    //declaro una variable, que se llama ubicación
+    //y cuyo valor es window.location.href
+    let ubicacion = window.location.href;
+    console.log(window.location.href);
+    console.log(ubicacion);
+    let ancho = window.innerWidth;
+    console.log(window.innerWidth);
+    let alto = window.innerHeight;
+    console.log(window.innerHeight);
+}
--- practicaincial/js/index.js
+++ practicaincial/js/index.js
@@ -1,5 +1,19 @@
+//esto es un comentario
 //window es una variable
 //predefinida que representa la ventana del navegador
 //y alert, es una función de window, que permite
 //mostrar mensajes en formato pop up
-window.alert("HOLA SOCIO"); // esto es un mensaje que sale por la ventana pop up
+//window.alert("HOLA SOCIO"); // esto es un mensaje que sale por la ventana pop up    
+console.log("visitando la página");
+
+//PARA QUÉ SIRVA JAVASCRIPT
+/*
+CAMBIAR LOS ESTILOS CSS DINÁMICAMENTE
+OCULTAR ELEMENTOS WEB DE LA PÁGINA
+AÑADIR ELEMENTOS WEB DE LA PÁGINA
+MODIFICAR EL CONTENIDO DE LA PÁGINA
+REACCIONAR A EVENTOS
+    EVENTOS DEL USUARIO
+    EVENTOS DEL NAVEGADOR
+PARA COMUNICARME CON EL SERVIDOR
+*/
