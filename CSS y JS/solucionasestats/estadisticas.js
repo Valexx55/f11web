8815c3999f7642a2bc96fdada3af4ee5050133e0
Val <valexx55@gmail.com>
2026-05-18 10:18:49 +0200

    estadisticas auto rellenado

--- CSS y JS/solucionasestats/estadisticas.js
+++ CSS y JS/solucionasestats/estadisticas.js
@@ -1,4 +1,5 @@
 //objeto (es una variable)
+//FORMATO JSON - es una forma de estructurar el texto, que permite estrucuturar los datos
 let estadisticasRemates = {
     titulo: "REMATES",
     equipoLocal: {
@@ -31,3 +32,20 @@ console.log(estadisticasRemates.titulo);
 console.log(estadisticasRemates.equipoLocal.nombre);
 //remates a puerta del equipo visitante
 console.log(estadisticasRemates.equipoVisitantes.remates.apuerta);
+
+//TODO: RELLENAR LA PÁGINA HTLM CON LOS DATOS DE estadisticasRemates
+
+//CUANDO se carga la página completa, salta
+//el evento onload
+window.onload = function () {
+    //en este momento ya se ha cargado todo el html
+    //las imágenes, el css, todo
+    console.log('Se ha cargado la página');
+    let imgLocal = document.getElementById('logolocal');
+    imgLocal.src = estadisticasRemates.equipoLocal.escudo;
+    imgLocal.alt = estadisticasRemates.equipoLocal.nombre;
+
+    let spanBloqLocal = document.getElementById('bloqueadosLocal');
+    spanBloqLocal.textContent = estadisticasRemates.equipoLocal.remates.bloqueados;
+
+}
