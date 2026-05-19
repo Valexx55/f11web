492b592bc69f023d34975358853cf0bdb4eea005
Val <valexx55@gmail.com>
2026-05-19 13:54:43 +0200

    dni 1/2

--- JS/dni/index.js
+++ JS/dni/index.js
@@ -1,3 +1,28 @@
+const SECUENCIA_LETRAS_DNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+
 function validarDni() {
     console.log('ha tocado el botón de validar');
+    //1 obtener numero, prefijo, letra
+    let numero = document.getElementById('numero').value; 
+    console.log('numero intro = ' + numero);
+    let letra = document.getElementById('letra').value; 
+    console.log('letra intro = ' + letra);
+    //2 calculo de la letra
+    let resto = numero % 23;
+    console.log('resto = ' + resto);
+    //3 comprobamos la letra 
+    let letraCalculada = SECUENCIA_LETRAS_DNI.charAt(resto);
+    console.log('Letra calculada = ' + letraCalculada);
+    //4 informar
+    if (letra==letraCalculada){
+        window.alert("DNI CORRECTO bien :)!");
+    }else {
+        window.alert("DNI INCORRECTO mal :)!");
+    }
+
+
+
+
+
+
 }
