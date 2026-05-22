0f967a5c3cd4e13e6593c729160f6b348286b076
Val <valexx55@gmail.com>
2026-05-22 13:51:09 +0200

    for divs

--- JS/juegotablero/soljosemari/index.js
+++ JS/juegotablero/soljosemari/index.js
@@ -5,4 +5,26 @@ function volverNegro(idDelCuadro) {
     
     // 2. Le cambiamos el color de fondo a negro
     elCuadroQueTocaste.style.backgroundColor = "black";
+    
+    if (estanTodosEnNegro())
+    {
+        alert("FIN DEL JUEGO")
+    } else {
+        console.log("No están todos en negro");
+    }
+
+}
+
+function estanTodosEnNegro ()
+{
+    let todosEnNegro = false;
+
+        let listaDivs = document.getElementsByClassName('cuadrito');
+        //listaDivs.length
+        for (let n = 0; n < listaDivs.length; n++)
+        {
+            console.log('Color ' + n + ' es ' + listaDivs[n].style.backgroundColor);
+        }
+
+    return todosEnNegro;
 }
