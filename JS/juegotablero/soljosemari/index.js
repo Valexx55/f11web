39e75b1adef3cd412453bcea691e4b345f1b70e9
Val <valexx55@gmail.com>
2026-05-25 13:49:56 +0200

    fin set interval

--- JS/juegotablero/soljosemari/index.js
+++ JS/juegotablero/soljosemari/index.js
@@ -28,3 +28,89 @@ function estanTodosEnNegro ()
 
     return todosEnNegro;
 }
+
+window.onload = pintarNumeracion;//sin paréntesis, se programa la llamada futura    
+
+function pintarNumeracion ()
+{
+    //recorrer los divs y pintar un número en cada uno
+   let cuadritos = document.getElementsByClassName('cuadrito');
+
+   for (let n = 0; n < cuadritos.length; n++)
+   {
+     cuadritos[n].textContent = n+1;
+   }
+
+   pintarCuadradosEnRosa ();
+}
+
+
+function pintarCuadradosEnRosa ()
+{
+    //recorrer los divs y pintar un número en cada uno
+   let cuadritos = document.getElementsByClassName('cuadrito');
+
+   for (let n = 0; n < cuadritos.length; n++)
+   {
+     if (n%2==0)
+     {
+        cuadritos[n].style.backgroundColor='pink';
+     } else {
+        cuadritos[n].style.backgroundColor='white';
+     }
+   }
+
+}
+
+function pintarCuadradosEnAzul ()
+{
+    //recorrer los divs y pintar un número en cada uno
+   let cuadritos = document.getElementsByClassName('cuadrito');
+
+    for (let n = 0; n < cuadritos.length; n++)
+   {
+     if (n%2==0)
+     {
+        cuadritos[n].style.backgroundColor='white';
+     } else {
+        cuadritos[n].style.backgroundColor='blue';
+     }
+   }
+
+}
+
+let esTableroRosa = true;
+let contadorCambios = 0;
+//setInterval(saluda, 7000);
+let idAlarma = setInterval(cambiarColor, 3000);
+
+function cambiarColor ()
+{
+  if (esTableroRosa){
+    pintarCuadradosEnAzul();
+    esTableroRosa=false;
+    contadorCambios=contadorCambios+1;
+  } else {
+    pintarCuadradosEnRosa();
+    esTableroRosa=true;
+  }
+  if (contadorCambios==3)
+  {
+    //TODO: parar el intervalo
+    clearInterval(idAlarma);
+  }
+}
+
+
+function saluda ()
+{
+    alert("HOLA QUÉ pasa :)")
+}
+
+
+
+
+
+
+
+
